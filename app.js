2011a81a9c73d4aadb6a7d068d6704af98f802a2
Carlos J Guzman <152306129+cjavier7@users.noreply.github.com>
2024-01-04 18:00:13 +0000

    proyecto pruebas unitarias

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}
+
+const fromDollarToYen = function(valueInDollar) {
+// convertimos el valor a yenes
+let valueInYen = valueInDollar * 156.5;
+// Retornamos el valor en yenes
+return valueInYen;
+
+}
+
+const fromYenToPound = function(valueInYen) {
+
+    let valueInPound = valueInYen * 0.87;
+    
+    return valueInPound;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const sum = (a,b) => {
     return a+b
@@ -6,6 +44,6 @@ const sum = (a,b) => {
 
 console.log(sum(7,3))
 
-module.exports = { sum };
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
 
 
--- test.js
+++ test.js
@@ -9,3 +9,41 @@ test('adds 14 + 9 to equal 23', () => {
     // Esperamos que la suma de esos 2 números sea 23
     expect(total).toBe(23);
 });
+
+
+test("One euro should be 1.07 dollars", function() {
+    // Importo la funcion desde app.js
+    const { fromEuroToDollar } = require('./app.js');
+
+    // Uso la función como debe ser usada
+    const dollars = fromEuroToDollar(3.5);
+
+    // Si 1 euro son 1.07 dólares, entonces 3.5 euros debe ser (3.5 * 1.07)
+    const expected = 3.5 * 1.07;
+
+    // Hago mi comparación (la prueba)
+    expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+})
+
+test("One dollar should be 156.5 yen", function() {
+
+    const { fromDollarToYen } = require('./app.js');
+
+    const yen = fromDollarToYen (7.2);
+ 
+    const expected = 7.2 * 156.5;
+
+    expect(fromDollarToYen(7.2)).toBe(1126.8);
+})
+
+
+test("One yen should be 0.87 pound", function() {
+
+    const { fromYenToPound } = require('./app.js');
+
+    const pound = fromYenToPound (1);
+ 
+    const expected = 1 * 0.87;
+
+    expect(fromYenToPound(1)).toBe(0.87);
+})
